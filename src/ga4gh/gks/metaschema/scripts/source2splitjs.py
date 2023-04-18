771b41debad17ab9a5c0bbd264ed867a72c7a7cd
Alex H. Wagner, PhD <Alex.Wagner@nationwidechildrens.org>
2023-04-18 08:22:45 -0400

    add split method to script

--- src/ga4gh/gks/metaschema/scripts/source2splitjs.py
+++ src/ga4gh/gks/metaschema/scripts/source2splitjs.py
@@ -6,5 +6,4 @@ from ga4gh.gks.metaschema.tools.source_proc import YamlSchemaProcessor
 
 source_file = pathlib.Path(sys.argv[1])
 p = YamlSchemaProcessor(source_file)
-root = source_file.parent
-
+p.split_defs_to_js()
